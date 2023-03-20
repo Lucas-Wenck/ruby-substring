3ac84b6d022828e1625f79134c341139fa06a24f
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-20 19:21:42 -0300

    Put string to lowercase, count value by key

--- substring.rb
+++ substring.rb
@@ -1,7 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substring(dictionary)
+def substring(string, dictionary)
+    string = string.downcase
     dictionary = Hash[dictionary.zip([0] * dictionary.length)]
+    dictionary.each_key do |substring|
+        count = string.scan(substring).length
+        dictionary[substring] = count
+    end
+    puts dictionary.reject { |key, value| value == 0}
 end
 
-substring(dictionary)
+substring("Howdy partner, sit down! How's it going?", dictionary)
