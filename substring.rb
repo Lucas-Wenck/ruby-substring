a6694468e3187a3b809592205d2a1a25778776c7
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-20 19:12:42 -0300

    Add method to transform array into hash with 0 value

--- substring.rb
+++ substring.rb
@@ -1,5 +1,7 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substring(string, dictionary)
-    
+def substring(dictionary)
+    dictionary = Hash[dictionary.zip([0] * dictionary.length)]
 end
+
+substring(dictionary)
